24b36c68e2709433873f828a896d0058460a099e
tom <tommaso@pavese.info>
2013-05-19 20:47:20 +0200

    converted indentation from tabs to spaces

--- tricks.js
+++ tricks.js
@@ -5,36 +5,36 @@ pic.src = 'bo_play_pattern.png';
 
 
 $(function(){
-	$(".trigger").click(function(e){
-		var inner = $(this).parent().find(".inner");
-		var box   = $(this).parent().find(".box");
+    $(".trigger").click(function(e){
+        var inner = $(this).parent().find(".inner");
+        var box   = $(this).parent().find(".box");
 
-		if(inner.css("height") === "0px"){
-			box.addClass("expanded");
-			inner.animate({height: "200px"}, 600);
-		}
-		else {
-			inner.animate({height: "0px"}, 600,
-				function(){
-					box.removeClass("expanded");
-				});
-		}
-	});
+        if(inner.css("height") === "0px"){
+            box.addClass("expanded");
+            inner.animate({height: "200px"}, 600);
+        }
+        else {
+            inner.animate({height: "0px"}, 600,
+                function(){
+                    box.removeClass("expanded");
+                });
+        }
+    });
 
 
 
-	$(".trigger2").click(function(e){
-		var box   = $(this).parent().find(".box");
+    $(".trigger2").click(function(e){
+        var box   = $(this).parent().find(".box");
 
-		if(box.css("height") === "0px"){
-			box.addClass("expanded");
-			box.animate({height: "200px"}, 600);
-		}
-		else {
-			box.animate({height: "0px"}, 600,
-				function(){
-					box.removeClass("expanded");
-				});
-		}
-	});
+        if(box.css("height") === "0px"){
+            box.addClass("expanded");
+            box.animate({height: "200px"}, 600);
+        }
+        else {
+            box.animate({height: "0px"}, 600,
+                function(){
+                    box.removeClass("expanded");
+                });
+        }
+    });
 });
